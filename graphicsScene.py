02b58664c622c57b64d94605ffb612b1eae7b256
Laura Ekstrand <laura@jlekstrand.net>
2016-11-17 15:40:30 -0800

    Initial commit

---
+++ graphicsScene.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3.5
+#Always run this with python3.5
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QGraphicsView, QGraphicsScene)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QFont, QColor
+
+class Example(QGraphicsView):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.scene = QGraphicsScene()
+        self.pixmap = QPixmap("geo5.jpg")
+        self.font = QFont("PT Sans", 16)
+        self.font.setBold(True)
+
+        #self.scene.addPixmap(self.pixmap.scaled(300, 200))
+        self.text = self.scene.addText("Praise to the Lord\nThe Almighty\nThe King of Creation", self.font)
+        self.text.setDefaultTextColor(QColor(255, 255, 255))
+
+        self.setScene(self.scene)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Graphics View')
+        self.show()
+
+    def keyPressEvent(self, e):
+
+        if e.key() == Qt.Key_Up:
+            self.rotate(30)
+
+        elif e.key() == Qt.Key_Down:
+            self.rotate(-30)
+
+    def drawBackground(self, qp, rect):
+        qp.drawPixmap(rect.toRect(), self.pixmap)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
