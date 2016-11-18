6a08e7a7dc79b857404949bb06b6dd301914b74f
Laura Ekstrand <laura@jlekstrand.net>
2016-11-17 22:42:05 -0800

    Playing around with built-in effects.

--- graphicsScene.py
+++ graphicsScene.py
@@ -2,7 +2,9 @@
 #Always run this with python3.5
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QGraphicsView, QGraphicsScene)
+from PyQt5.QtWidgets import (QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem,
+                             QGraphicsBlurEffect, QGraphicsOpacityEffect, QGraphicsDropShadowEffect,
+                             QGraphicsColorizeEffect)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QFont, QColor
 
@@ -24,6 +26,9 @@ class Example(QGraphicsView):
         #self.scene.addPixmap(self.pixmap.scaled(300, 200))
         self.text = self.scene.addText("Praise to the Lord\nThe Almighty\nThe King of Creation", self.font)
         self.text.setDefaultTextColor(QColor(255, 255, 255))
+        self.text.setFlags(QGraphicsItem.ItemIsSelectable |
+                           QGraphicsItem.ItemIsMovable)
+        self.text.setTextInteractionFlags(Qt.TextEditable)
 
         self.setScene(self.scene)
 
@@ -38,6 +43,28 @@ class Example(QGraphicsView):
 
         elif e.key() == Qt.Key_Down:
             self.rotate(-30)
+
+        elif e.key() == Qt.Key_B:
+
+            self.text.setGraphicsEffect(QGraphicsBlurEffect())
+
+        elif e.key() == Qt.Key_S:
+
+            shadow = QGraphicsDropShadowEffect()
+            shadow.setOffset(2)
+            shadow.setBlurRadius(5)
+            self.text.setGraphicsEffect(shadow)
+
+        elif e.key() == Qt.Key_C:
+
+            self.text.setGraphicsEffect(QGraphicsColorizeEffect())
+
+        elif e.key() == Qt.Key_O:
+
+            self.text.setGraphicsEffect(QGraphicsOpacityEffect())
+
+        elif e.key() == Qt.Key_N:
+                self.text.setGraphicsEffect(None)
 
     def drawBackground(self, qp, rect):
         qp.drawPixmap(rect.toRect(), self.pixmap)
